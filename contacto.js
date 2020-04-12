d5f03a78fca17a36681c6a8d266d3a0c0c6d1fcc
Mariela Vallecillo <mvalletelt@gmail.com>
2020-04-12 10:31:03 -0300

    agregué la pestaña contacto

---
+++ contacto.js
@@ -0,0 +1,11 @@
+
+
+function contacto (){
+    const titulo = '\nContáctanos';
+
+    const contenido= '\n\n ¿Tenés algo para contarnos?. Nos encanta escuchar a nuestros clientes.\n\n  Si deseas contactarnos podés escribirnos al siguiente email:\n\n  - dhmovies@digitalhouse.com o en las redes sociales \n\nEnvíanos tu consulta, sugerencia  o reclamo y será respondida a la brevedad posible.\nRecordá que también podés consultar la sección de Preguntas Frecuentes para obtener respuestas inmediatas a los problemas más comunes.'
+    
+
+
+    return titulo + contenido
+}
